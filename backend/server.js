aed3da6bb75a3446bff9303944302daf008a3fc2
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2022-05-05 04:14:57 +0300

    add server port

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,14 @@
+// here we defined the server configuration and the port, etc.
+// keep in mind that the server.js is the starting point it's like th index or main method
+const express = require('express')
+const dotenv = require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
 
-console.log("hello world");
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
